30fa5cf917a8df1ff8e7225f6077c4d0bea790ed
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-03-07 15:34:35 +0000

    Create test for initial application loading

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from petrol_locations import app
+from flask.testing import FlaskClient
+
+def test_app_running():
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
+
+def test_app_visible():
+    with app.test_client() as client:
+        response = client.get("/")
+        assert b'Hello World' in response.data
