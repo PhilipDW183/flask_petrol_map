1ecea3c46a2dcdbda2569fbf2f0114012a01d6be
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-03-09 16:43:39 +0000

    Add fuel location form

---
+++ petrol_locations/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired, Length, Regexp
+import re
+
+FUEL_TYPES = ["Diesel","Octane 91","Octane 95","Octane 98","E10","LPG", "LH2"]
+
+class PetrolForm(FlaskForm):
+    postcode = StringField("Postcode",
+                           validators = [DataRequired(),
+                                         Length(min=5, max=8,
+                                                message="Should be 5 to 7 letters"),
+                                        Regexp(regex=r'\b[A-Z]{1,2}[0-9][A-Z0-9]? [0-9][ABD-HJLNP-UW-Z]{2}\b',
+                                               flags=re.IGNORECASE,
+                                               message="Not a recognised postcode")] )
+    fuel_type = SelectField("Fuel Type",
+                            choices = ((fuel, fuel) for fuel in FUEL_TYPES),
+                            validators = [DataRequired()])
+    
--- petrol_locations/routes.py
+++ petrol_locations/routes.py
@@ -1,6 +1,8 @@
 from petrol_locations import app
+from petrol_locations.forms import PetrolForm
 from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    form = PetrolForm()
+    return render_template("index.html", form=form)
