4e177e0ea3d0994f440421e320e4947779ac87d5
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-03-12 11:40:34 +0000

    Update app testing for CSRF

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,19 @@
 from petrol_locations import app
 from flask.testing import FlaskClient
+import pytest
 
-def test_app_running():
-    with app.test_client() as client:
+@pytest.fixture
+def setup_app():
+    app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
+    return app
+
+def test_app_running(setup_app):
+    with setup_app.test_client() as client:
         response = client.get("/")
         assert response.status_code == 200
 
-def test_app_visible():
-    with app.test_client() as client:
+def test_app_visible(setup_app):
+    with setup_app.test_client() as client:
         response = client.get("/")
         assert b'Petrol station map' in response.data
