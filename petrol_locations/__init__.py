c10afea1eaa1d4219680252460ff7d1c3edc57b1
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-03-13 13:43:23 +0000

    Integrate geocode function into route

--- petrol_locations/__init__.py
+++ petrol_locations/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(Config())
 
 from petrol_locations import routes
--- petrol_locations/routes.py
+++ petrol_locations/routes.py
@@ -1,11 +1,20 @@
 from petrol_locations import app
 from petrol_locations.forms import PetrolForm
-from petrol_locations.helpers import create_base_map, render_map
-from flask import render_template
+from petrol_locations.helpers import create_base_map, render_map, geocode_postcode
+from flask import render_template, flash
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     form = PetrolForm()
     map_obj = create_base_map()
     iframe = render_map(map_obj)
+    if form.validate_on_submit():
+        postcode = form.postcode.data
+        fuel_type = form.fuel_type.data
+        location = geocode_postcode(postcode)
+        if not location:
+            flash("Postcode not found", "danger")
+            render_template("index.html", form=form, map=iframe)
+        else:
+            render_template("index.html", form=form, map=iframe)
     return render_template("index.html", form=form, map=iframe)
